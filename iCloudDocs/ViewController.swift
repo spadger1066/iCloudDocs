49a0d4041032db579651ab9b7826d6592dc93391
Stephen Maitland <s.maitland@me.com>
2016-02-01 18:55:17 +0000

    Initial Commit

---
+++ iCloudDocs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iCloudDocs
+//
+//  Created by Stephen on 01/02/2016.
+//  Copyright © 2016 luminator.technology.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
